ad2ee6fccdcb6200e5650cdcf914c0d67401df1f
Brian <brianmunoz240@gmail.com>
2022-03-10 14:13:09 -0600

    se corrigio el error de valores duplicados

--- Sistemas_Expertos/v2.py
+++ Sistemas_Expertos/v2.py
@@ -2,16 +2,27 @@
 objetos = {
     '1': ['A', 'B', 'C'],
     '2': ['A', 'M', 'Y'],
-    '3': ['A', 'B', 'D'],
-    '4': ['D', 'X', 'C']
+    '3': ['D', 'X', 'C'],
+    '4': ['A', 'B', 'D']
 }
 tiene = []
-def sistema_experto_ver2(objetos,tiene): #diccionario , lista vacia
+no_tiene = []
+def sistema_experto_ver2(objetos,tiene,no_tiene): #diccionario , lista vacia
     for i in range(len(objetos)):
         for j in range(len(objetos[str(i+1)])):
-            if objetos[str(i+1)][j] not in tiene :
+            if objetos[str(i+1)][j] not in tiene and objetos[str(i+1)][j] not in no_tiene  :
                 if input(f'Tiene {objetos[str(i+1)][j]} : ') =='si':
                     tiene.append(objetos[str(i+1)][j])
-        if tiene == objetos[str(i+1)]:
+                else:
+                    no_tiene.append(objetos[str(i+1)][j])
+
+        if sorted(tiene) == sorted(objetos[str(i+1)]):
             return ( f'tu objeto es el objeto {i+1}')
-print(sistema_experto_ver2(objetos,tiene))
+print(sistema_experto_ver2(objetos,tiene, no_tiene))
+
+
+
+
+
+
+
